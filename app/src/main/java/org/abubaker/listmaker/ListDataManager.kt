57fdac446dcbd841182d56d64be7b66f46c068f6
abubaker <mohammad_abubaker@yahoo.com>
2021-02-03 02:28:59 +0500

    adding comments to the ListDataManager.kt file

--- app/src/main/java/org/abubaker/listmaker/ListDataManager.kt
+++ app/src/main/java/org/abubaker/listmaker/ListDataManager.kt
@@ -12,7 +12,7 @@ class ListDataManager(private val context: Context) {
     // saveLists
     fun saveList(list: TaskList) {
 
-        // Here we are asking to get write-able access to the app's default SharedPreference store
+        // Here we are asking to get access to WRITE to the app's default SharedPreference store
         // This will allow us to write key-value pairs
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context).edit()
 
@@ -28,23 +28,29 @@ class ListDataManager(private val context: Context) {
     // readLists()
     fun readLists(): ArrayList<TaskList> {
 
-        //
+        // Here we are asking to get READ-ONLY access to the app's default SharedPreference store
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
-        //
+        // Get store's content as a MAP Object (i.e Dictionary, as it will contain UNIQUE keys)
         val sharedPreferenceContents = sharedPreferences.all
 
-        //
+        // We are creating taskLists (an empty ArrayList) that will be used to store retrieved data
         val taskLists = ArrayList<TaskList>()
 
-        //
         for (taskList in sharedPreferenceContents) {
+
+            // CONVERSION: Value will be casted as a HashSet<String>
+            // as TaskList cannot be directly stored as a String
             val itemsHashSet = ArrayList(taskList.value as HashSet<String>)
+
+            // We will recreate the TaskList by passing the KEY and the Value
             val list = TaskList(taskList.key, itemsHashSet)
 
-            //
+            // We will add content from list to the taskLists (array)
             taskLists.add(list)
         }
+
+        // Return the contents of taskLists to the caller of the method.
         return taskLists
     }
 
