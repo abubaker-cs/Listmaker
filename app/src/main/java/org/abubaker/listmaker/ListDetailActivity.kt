86f7f1ae96801a8426f675b565099d019a9530ff
abubaker <mohammad_abubaker@yahoo.com>
2021-02-03 17:13:23 +0500

    working...

--- app/src/main/java/org/abubaker/listmaker/ListDetailActivity.kt
+++ app/src/main/java/org/abubaker/listmaker/ListDetailActivity.kt
@@ -2,8 +2,12 @@ package org.abubaker.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListDetailActivity : AppCompatActivity() {
 
@@ -12,6 +16,9 @@ class ListDetailActivity : AppCompatActivity() {
 
     // It will be a reference to our RecyclerView in our XML file
     lateinit var listItemsRecyclerView: RecyclerView
+
+    //
+    lateinit var addTaskButton: FloatingActionButton
 
     // onCreate()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +41,42 @@ class ListDetailActivity : AppCompatActivity() {
 
         // We are asking the LinearLayoutManager to handle the presentational responsibilities
         listItemsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        // FAB
+        addTaskButton = findViewById(R.id.add_task_button)
+        addTaskButton.setOnClickListener {
+            showCreateTaskDialog()
+        }
+    }
+
+    // Custom Dialog
+    private fun showCreateTaskDialog() {
+
+        // Inserting a inputField to collect data from the user
+        val taskEditText = EditText(this)
+
+        // Defining the TYPE of inputField so proper KEYBOARD can be shown
+        taskEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.task_to_add)
+            .setView(taskEditText)
+            .setPositiveButton(R.string.add_task) { dialog, _ ->
+
+                val task = taskEditText.text.toString()
+                list.tasks.add(task)
+
+                val recyclerAdapter =
+                    listItemsRecyclerView.adapter as ListSelectionRecyclerViewAdapter
+
+                recyclerAdapter.notifyItemInserted(list.tasks.size - 1)
+
+                // We will later on add code here to preserve user's submitted data.
+                dialog.dismiss()
+
+            }
+
+
     }
 
 
