495ac7bf3faa4eff5d4758fae5812de461d38693
abubaker <mohammad_abubaker@yahoo.com>
2021-02-03 14:04:49 +0500

    We will be using companion object to meet the protocol of Parcels, as Kotlin does not support static methods.

--- app/src/main/java/org/abubaker/listmaker/TaskList.kt
+++ app/src/main/java/org/abubaker/listmaker/TaskList.kt
@@ -9,22 +9,26 @@ class TaskList constructor(
     val tasks: ArrayList<String> = ArrayList()
 ) : Parcelable {
 
-    //
+    // Reading from the Parcel
+    // Read received data, and create a local ArrayList<> of Strings
     constructor(source: Parcel) : this(
-        source.readString()!!,
-        source.createStringArrayList()!!
+        source.readString()!!, source.createStringArrayList()!!
     )
 
     //
     override fun describeContents() = 0
 
-    //
+    // Write to the Parcel
     override fun writeToParcel(dest: Parcel, flags: Int) {
         dest.writeString(name)
         dest.writeStringList(tasks)
     }
 
-    //
+    // ISSUE: Kotlin does not have "static" method, so we will create a "companion object"
+    // ==================================================================================
+    // We cannot create a static method in Kotlin, as it is done in Java using:
+    // public static Parcelable.Creator<T> CREATOR so, to meet the meet the protocol's requirements,
+    // we will instead create a "companion object" in kotlin and override appropriate functions.
     companion object CREATOR : Parcelable.Creator<TaskList> {
 
         //
